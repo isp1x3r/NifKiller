bced6e965f2ecc424942fbf8aabae3131ac35f18
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-10-14 13:59:04 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace NifKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proccesses = Process.GetProcessesByName("BlackShot");
+            int bsProcID = proccesses.FirstOrDefault().Id;
+            if (bsProcID == 0)
+            {
+                Console.WriteLine("Failed To Find BlackShot Process");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            string handlepath = Directory.GetCurrentDirectory() + "\\handle.exe";
+            Console.WriteLine(handlepath);
+            // Query nif handle info
+            if(!File.Exists(handlepath))
+            {
+                Console.WriteLine("Cannot find handle utility");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            string reply = Util.runCommand(handlepath, "-p BlackShot.exe");
+            // Parse The Info
+            List<string> _HandleIds = new List<string>();
+            using (StringReader reader = new StringReader(reply))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    if (line.Contains(".nif"))
+                    {
+                        char[] Id = new char[3];
+                        line.CopyTo(2, Id, 0, 3);
+                        _HandleIds.Add(new string(Id));
+                    }
+                }
+                if(_HandleIds.Count == 0)
+                {
+                    Console.WriteLine("Cannot find nif handles");
+                    System.Threading.Thread.Sleep(4000);
+                    Environment.Exit(0);
+
+                }
+                // Now that we have the nif handle ids we just call handle.exe with proper arguments and just close them
+                foreach (string handleid in _HandleIds)
+                {;
+                    Util.runCommand(handlepath, "-c 0x" + handleid + " -y -p " + bsProcID);
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Successfully Killed All Nif Handles!");
+                Console.ResetColor();
+                System.Threading.Thread.Sleep(4000);
+            }
+        }
+    }
+}
+
---
+++ Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NifKiller
+{
+    internal static class Util
+    {
+        internal static string runCommand(string filename, string argument)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = filename;
+            process.StartInfo.Arguments = argument; // Note the /c command (*)
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = false;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+
+    }
+}
